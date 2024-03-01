c0007d5b5568a007cb6e1390cd331795b83afbf5
unknown <akswns03@naevr.com>
2024-03-02 00:26:45 +0900

    주석 추가

--- BubbleSort.mjs
+++ BubbleSort.mjs
@@ -1,7 +1,7 @@
 const bubbleSort = (arr) => {
     for(let i = 0; i < arr.length - 1; i++) { 
         for(let j = 0; j < (arr.length - i - 1); j++) { // 정렬이 완료된 갯수만큼 빼줌
-            if(arr[j] > arr[j+1]){
+            if(arr[j] > arr[j+1]){ //배열의 두 숫자를 비교
                 let temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
