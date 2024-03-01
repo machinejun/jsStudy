8f23d4a19a2972cd93f178c36eef1b90d0425b3f
unknown <akswns03@naevr.com>
2024-03-02 00:24:41 +0900

    bubble Sort

---
+++ BubbleSort.mjs
@@ -0,0 +1,16 @@
+const bubbleSort = (arr) => {
+    for(let i = 0; i < arr.length - 1; i++) { 
+        for(let j = 0; j < (arr.length - i - 1); j++) { // 정렬이 완료된 갯수만큼 빼줌
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+const arr = [5, 2, 9, 6, 8, 3];
+console.log("before : :" + arr);
+bubbleSort(arr);
+console.log("after : :" + arr);
