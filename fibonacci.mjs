9ee5ac487761e213f1b2521bf714f8b62330606c
junic <akswns03@naver.com>
2024-03-06 22:10:47 +0900

    타뷰레이션

--- fibonacci.mjs
+++ fibonacci.mjs
@@ -26,24 +26,50 @@ const upgradeFibonacci = (n, memo) => {
     return memo[n];
 }
 
+const upgrade2Fibonacci = (n) => {
+    if(n == 0 || n == 1) return n;
 
-const checkTime = (n, memo) => {
+    let table = [0, 1];
+
+    for(let i = 2; i <= n; i++){
+        table[i] = table[i - 2] + table[i - 1];
+    }
+    return table[n];
+}
+
+const logMemoryUsage = () => {
+    const memoryUsage = process.memoryUsage();
+    console.log("Resident Set Size: " + memoryUsage.rss / (1024 * 1024) + " MB");
+    console.log("Heap Total: " + memoryUsage.heapTotal / (1024 * 1024) + " MB");
+    console.log("Heap Used: " + memoryUsage.heapUsed / (1024 * 1024) + " MB");
+}
+
+const checkTime = (n, version, memo) => {
     const startTime = performance.now();
-    const endTime = performance.now();
     let result;
-    if(memo){
-        result = upgradeFibonacci(n, memo);
-    } else {
-        result = fibonacci(n);
-    } 
+
+    switch(version) {
+        case 0 :
+            result = fibonacci(n)
+        break;
+        case 1 :
+            result = upgradeFibonacci(n, memo)
+        break;  
+        case 2 :
+            result = upgrade2Fibonacci(n)
+    }
+    const endTime = performance.now();
     const executionTime = (endTime - startTime);
 
     console.log("소요시간 : " + executionTime );
     console.log("결과 : " + result);
+    //logMemoryUsage();
 } 
 
 console.log("-------------fibonacci-------------")
-checkTime(30); // O(2^n) 성능
+checkTime(30, 0); // O(2^n) 성능
 console.log();
 console.log("-------------upgrade fibonacci-------------")
-checkTime(100, {}); // O(n) 성능
+checkTime(40,1, {}); // O(n) 성능 
+console.log("-------------upgrade fibonacci2-------------")
+checkTime(40, 2);
